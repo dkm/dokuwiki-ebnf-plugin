f5f90b2d55d9612bf5c3e88dd85438470ccf5a92
Marc Poulhiès <dkm@kataplop.net>
2010-01-28 15:00:41 +0100

    add event when clicking ebnf image to toggle their size

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-function addLoadEvent(func) {
-  var oldonload = window.onload;
-  if (typeof window.onload != 'function') {
-    window.onload = func;
-  } else {
-    window.onload = function() {
-      if (oldonload) {
-        oldonload();
-      }
-      func();
-    }
-  }
+
+var original_size = [];
+var ebnf_imgs
+function addEvent(obj, evType, fn){
+ if (obj.addEventListener){
+   obj.addEventListener(evType, fn, false);
+   return true;
+ } else if (obj.attachEvent){
+   var r = obj.attachEvent("on"+evType, fn);
+   return r;
+ } else {
+   return false;
+ }
 }
 
-// function getElementsByClassName(classname, node)  {
-//     if(!node) node = document.getElementsByTagName("body")[0];
-//     var a = [];
-//     var re = new RegExp('\\b' + classname + '\\b');
-//     var els = node.getElementsByTagName("*");
-//     for(var i=0,j=els.length; i<j; i++)
-//         if(re.test(els[i].className))a.push(els[i]);
-//     return a;
-// }
+function toggleImg(i){
+  var tmp = this.width;
+  this.width=this.owidth;
+  this.owidth = tmp;
+}
 
 function resizeIfNeeded(){
   var ebnf_imgs = document.getElementsByClassName('ebnf');
   for (var i=ebnf_imgs.length-1; i>=0;--i) {
     var w = ebnf_imgs[i].width;
-    ebnf_imgs[i].width = Math.min(window.innerWidth-20, w);
-    ebnf_imgs[i].alt = "toto";
+    ebnf_imgs[i].owidth = w;
+    ebnf_imgs[i].width = Math.min(Math.round(window.innerWidth*0.90), w);
+    addEvent(ebnf_imgs[i], 'click', toggleImg)
   }
 }
 
-addLoadEvent(resizeIfNeeded);
+
+
+addEvent(window, 'load', resizeIfNeeded);
